1610ae668872384a4f62ebb5f671d5590783beba
breadchris <breadchris@users.noreply.github.com>
2023-01-21 21:10:43 +0000

    chore: update generated files

--- server/src/routes/operations/index.js
+++ server/src/routes/operations/index.js
@@ -3,9 +3,13 @@ import express from 'express'
 import auth from '../../core/auth.js'
 
 import exploitCVE202121315 from './exploitCVE202121315.js'
+import exploitCVE20134660 from './exploitCVE20134660.js'
+import exploitCVE202236067 from './exploitCVE202236067.js'
 
 const router = express.Router()
 
 router.post('/exploit-cve202121315',  auth,  exploitCVE202121315)
+router.post('/exploit-cve20134660',  auth,  exploitCVE20134660)
+router.post('/exploit-cve202236067',  auth,  exploitCVE202236067)
 
 export default router
--- web-app/src/router.js
+++ web-app/src/router.js
@@ -5,6 +5,8 @@ import createAuthRequiredPage from "./auth/pages/createAuthRequiredPage.js"
 
 import MainPage from "./ext-src/MainPage"
 import CVE202121315Page from "./ext-src/vulnerability/CVE202121315Page"
+import CVE20134660Page from "./ext-src/vulnerability/CVE20134660Page"
+import CVE202236067Page from "./ext-src/vulnerability/CVE202236067Page"
 
 
 const router = (
@@ -12,6 +14,8 @@ const router = (
     <div>
       <Route exact path="/" component={ MainPage }/>
       <Route exact path="/vulnerability/cve-2021-21315" component={ CVE202121315Page }/>
+      <Route exact path="/vulnerability/cve-2013-4660" component={ CVE20134660Page }/>
+      <Route exact path="/vulnerability/cve-2022-36067" component={ CVE202236067Page }/>
 
     </div>
   </Router>
